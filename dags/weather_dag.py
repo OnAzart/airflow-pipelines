e05ea91705fe3ef6d339ae9d291eeabdefb09660
nazar_tutyn1 <nazar.tutyn@grainger.com>
2023-04-15 11:30:22 +0300

    added transformation of historical data and corrected historical api

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -17,26 +17,35 @@ from airflow.utils.task_group import TaskGroup
 def choosing_endpoint(city, **context):
     scheduled_date_to_run = context['execution_date']
     if scheduled_date_to_run.date() == datetime.today().date():
-        return f'retrieve_weather_data_{city}'
+        return f'retrive_data_group_{city}.retrieve_weather_data_{city}'
     return f'retrive_data_group_{city}.retrieve_historical_weather_data_{city}'
 
 
 def transforming_data(city, **context):
     ti = context['ti']
     group_name = f'retrive_data_group_{city}'
-    info = ti.xcom_pull(task_ids=f'{group_name}.retrieve_weather_data_{city}') or \
-           ti.xcom_pull(task_ids=f'{group_name}.retrieve_historical_weather_data_{city}')
 
-    timestamp = info["dt"]
-    temp = info["main"]["temp"]
-    humidity = info["main"]["humidity"]
-    cloudiness = info["weather"][0]["description"]
-    wind_speed = info["wind"]["speed"]
-
+    weather_data = ti.xcom_pull(task_ids=f'{group_name}.retrieve_weather_data_{city}')
+    historical_weather_data = ti.xcom_pull(task_ids=f'{group_name}.retrieve_historical_weather_data_{city}')
+    if weather_data:
+        timestamp = weather_data["dt"]
+        temp = weather_data["main"]["temp"]
+        humidity = weather_data["main"]["humidity"]
+        cloudiness = weather_data["weather"][0]["description"]
+        wind_speed = weather_data["wind"]["speed"]
+    elif historical_weather_data:
+        timestamp = historical_weather_data["data"]["dt"]
+        temp = historical_weather_data["data"]["temp"]
+        humidity = historical_weather_data["data"]["humidity"]
+        cloudiness = historical_weather_data["data"]["weather"]["description"]
+        wind_speed = historical_weather_data["data"]["wind_speed"]
+    else:
+        # timestamp, temp, humidity, cloudiness, wind_speed = [None for _ in range(5)]
+        raise Exception("No data for transform")
     return timestamp, temp, humidity, cloudiness, wind_speed
 
 
-with DAG(dag_id='weather_dag', schedule_interval='0 12 * * *', start_date=days_ago(0), catchup=True) as dag:
+with DAG(dag_id='weather_dag', schedule_interval='* 23 * * *', start_date=days_ago(1), catchup=True) as dag:
     create_table = SqliteOperator(task_id='create_table',
                                   sqlite_conn_id='measure_db',
                                   sql='''CREATE TABLE IF NOT EXISTS measures_ext (
@@ -52,9 +61,7 @@ with DAG(dag_id='weather_dag', schedule_interval='0 12 * * *', start_date=days_a
     # cities = ['Lviv', 'Kyiv', 'Zhmerynka', 'Kharkiv', 'Odesa']
     cities_dict = {'Lviv': [49.8383, 24.0232], 'Kyiv':  [50.4333, 30.5167], 'Zhmerynka': [49.037, 28.112],
               'Kharkiv': [50, 36.25], 'Odesa': [46.4775, 30.7326]}
-    # 'https://history.openweathermap.org/data/2.5/history/city?id=2885679&type=hour&appid=&start=1681417427&cnt=1'
     for city, coordinates in cities_dict.items():
-        # postgres://nazar:1111@localhost:5432/airflow-pipelines
         choose_endpoint = BranchPythonOperator(task_id=f'choose_endpoint_{city}',
                                                python_callable=choosing_endpoint,
                                                op_kwargs={'city': city})
@@ -64,7 +71,9 @@ with DAG(dag_id='weather_dag', schedule_interval='0 12 * * *', start_date=days_a
                                                        method='GET',
                                                        http_conn_id='weather_http_api',
                                                        endpoint='data/2.5/weather',
-                                                       data={'q': f'{city}', 'appid': Variable.get('OPENWEATHER_API')},
+                                                       data={'q': f'{city}',
+                                                             'appid': Variable.get('OPENWEATHER_API'),
+                                                             'units': 'metric'},
                                                        response_filter=lambda x: json.loads(x.text),
                                                        log_response=True,
                                                        )
@@ -72,21 +81,17 @@ with DAG(dag_id='weather_dag', schedule_interval='0 12 * * *', start_date=days_a
             retrieve_historical_weather_data = SimpleHttpOperator(task_id=f'retrieve_historical_weather_data_{city}',
                                                                   method='GET',
                                                                   http_conn_id='weather_http_api',
-                                                                  endpoint='data/2.5/weather',
-                                                                  # endpoint='data/3.0/onecall/timemachine',
+                                                                  endpoint='data/3.0/onecall/timemachine',
                                                                   response_filter=lambda x: json.loads(x.text),
                                                                   log_response=True,
-                                                                  data={'q': f'{city}',
-                                                                        'appid': Variable.get('OPENWEATHER_API')},
-                                                                  # data={
-                                                                  #     # 'q': f'{city}',
-                                                                  #     'lat': coordinates[0],
-                                                                  #     'lon': coordinates[1],
-                                                                  #     'appid': Variable.get('OPENWEATHER_API'),
-                                                                  #     'dt': '{{ execution_date.timestamp()|int }}'
-                                                                  # }
+                                                                  data={
+                                                                      'lat': coordinates[0],
+                                                                      'lon': coordinates[1],
+                                                                      'appid': Variable.get('OPENWEATHER_API'),
+                                                                      'dt': '{{ execution_date.timestamp()|int }}',
+                                                                      'units': 'metric'
+                                                                  }
                                                                   )
-            # retrieve_weather_tasks = [retrieve_weather_data, retrieve_historical_weather_data]
 
         transform_data = PythonOperator(task_id=f'transform_data_{city}',
                                         python_callable=transforming_data,
